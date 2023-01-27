85e759f3b7ac6b40baf6ff64909af642b63256f1
EduardoAlphonse <eduardoliveira.dev@gmail.com>
2023-01-27 12:05:29 -0300

    Commit inicial, conversão do projeto em HTML/CSS/JS para Next.JS

---
+++ src/components/PetCard.tsx
@@ -0,0 +1,35 @@
+import { ArrowRight } from "phosphor-react";
+
+interface PetCardProps {
+  name: string;
+  breed: string;
+  photo: string;
+  age: string;
+}
+
+export const PetCard = ({ name, breed, photo, age }: PetCardProps) => {
+  return (
+    <a href="" className="flex flex-col group">
+      <img
+        src={photo}
+        alt={name}
+        className="w-full rounded-2xl h-52 object-cover"
+      />
+      <footer className="flex justify-between items-center">
+        <div className="mt-3">
+          <strong className="text-lg">{name}</strong>
+          <div className="text-xs text-zinc-600 mt-1">
+            <span className="relative pr-2 mr-2 after:absolute after:content-['\2022'] after:right-[-5px]">
+              {breed}
+            </span>
+            <span>{age}</span>
+          </div>
+        </div>
+
+        <button className="p-2 rounded-full bg-zinc-200 text-[0px] group-hover:bg-gradient-to-tr from-pink-500 via-purple-500 to-indigo-500 group-hover:text-white">
+          <ArrowRight className="text-lg" />
+        </button>
+      </footer>
+    </a>
+  );
+};
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
       <Head />
-      <body>
+      <body className="text-zinc-800 bg-zinc-100 pb-8">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
